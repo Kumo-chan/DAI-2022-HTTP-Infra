a8956ce1fa7156b23410cdbf66fe81fc0e747f50
Florian <florian.conti@heig-vd.ch>
2022-12-12 15:49:58 +0100

    update

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,39 +5,34 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send( generateStudents() );
+    res.send( generateAnimals() );
 });
 
 app.listen(3000, function () {
     console.log('Accept HTTP resquests on port 3000.');
 });
 
-function generateStudents() {
-    var numberOfStudents = chance.integer({
+function generateAnimals() {
+    var numberOfAnimals = chance.integer({
         min: 0,
         max: 10
     });
 
-    console.log(numberOfStudents);
-    var students = [];
+    console.log(numberOfAnimals);
+    var animals = [];
 
-    for (var i = 0; i < numberOfStudents; i++) {
-        var gender = chance.gender();
-        var birthYear = chance.year({
-            min: 1996,
-            max:2006
+    var types = ["ocean", "desert", "grassland", "forest", "farm", "pet", "zoo"];
+
+    for (var i = 0; i < numberOfAnimals; i++) {
+
+        var randomType = types[Math.floor(Math.random()*types.length)];
+
+        animals.push({
+            type: randomType,
+            animal: chance.animal({type: randomType}),
         });
-        students.push({
-            firstName: chance.first({
-                gender: gender,
-            }),
-            lastName: chance.last(),
-            gender: gender,
-            birthday: chance.birthday({
-                year: birthYear
-            })
-        });
-    };
-    console.log(students);
-    return students;
+
+    }
+    console.log(animals);
+    return animals;
 }
